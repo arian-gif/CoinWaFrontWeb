4ee6996ebc364c85d73b2140ee4872ae7cc70a1c
Arian khan 21078101 <as8khan@uwaterloo.ca>
2025-04-01 15:02:58 -0400

    build change

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -373,7 +373,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "7052ca82bc0c12d1d3edcf9bed39edb6
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "0c142dbfda13e19943017d850b9f9835",
+"flutter_bootstrap.js": "ed82df959d3d905b1b284c8c7bc34a98",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
